19544c6edda70ab249c583b4cf5a3a0ea079555f
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 19:18:35 -0300

    Git Downloader

---
+++ downloadFromGit.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const fileName = 'gitList.txt';
+	await ns.wget('https://api.github.com/repos/guigpm/bitburner/contents/', fileName);
+	const gitList = ns.read(fileName);
+	const gitData = JSON.parse(gitList);
+
+	for (const file of gitData) {
+		if (file.name.endsWith('.js')) {
+			await ns.wget(file.download_url, file.name);
+		}
+	}
+
+	ns.rm(fileName);
+}
