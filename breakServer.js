818f7288e45dc43b8ffb1972fcc555561c3799db
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 22:52:39 -0300

    breakServer.js with lib and log

--- breakServer.js
+++ breakServer.js
@@ -1,17 +1,12 @@
-/** @param {NS} ns */
-function log(ns, target, message) {
-	ns.tprint(target, " ", message);
-	ns.print(target, " ", message);
-}
+import { deploy } from 'lib.js';
+import { log } from 'log.js';
 
 export async function main(ns) {
 	ns.disableLog("sleep");
 	const executer = ns.args[0];
 	const target = ns.args[1];
-	await ns.scp("hack.js", executer);
-	await ns.scp("weak.js", executer);
-	await ns.scp("grow.js", executer);
-	await ns.scp("invade.js", target);
+	await deploy(ns, executer);
+	await deploy(ns, target);
 
 	var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
 	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
@@ -20,19 +15,19 @@ export async function main(ns) {
 	const availableRamTarget = ns.getServerMaxRam(target) - ns.getServerUsedRam(target);
 	const weakThreads = Math.floor(availableRam / ns.getScriptRam("weak.js"));
 	const growThreads = Math.floor(availableRam / ns.getScriptRam("grow.js"));
-	const invadeThreads = Math.floor(availableRamTarget / ns.getScriptRam("invade.js"));
+	const harvestThreads = 1;
 
 	let pid = 0;
 	while (true) {
 		if (ns.getServerSecurityLevel(target) > securityThresh) {
-			log(ns, executer, "Weak " + target)
+			log.info(ns, "Weak " + target, executer)
 			pid = ns.exec("weak.js", executer, weakThreads, target);
 		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-			log(ns, executer, "Grow " + target)
+			log.info(ns, "Grow " + target, executer)
 			pid = ns.exec("grow.js", executer, growThreads, target);
 		} else {
-			log(ns, executer, "Invade " + target)
-			pid = ns.exec("invade.js", target, invadeThreads, target);
+			log.info(ns, "Harvest " + target, executer)
+			pid = ns.exec("harvest.js", target, harvestThreads, target);
 			break;
 		}
 		while (ns.isRunning(pid, executer)) {
