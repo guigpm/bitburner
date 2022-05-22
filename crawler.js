9a14180c906730db0835e5844a48907dcec6962e
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 22:53:33 -0300

    crawler.js  with lib and log

--- crawler.js
+++ crawler.js
@@ -1,49 +1,77 @@
-/** @param {NS} ns */
+import { waitTargetPid, deploy } from 'lib.js';
+
+/**
+ * @param {NS} ns
+ * @param {string} target
+ */
 async function spread(ns, target) {
-	const pid = ns.exec("crawler.js", target, 1, ns.args[0]);
-	while(ns.isRunning(pid)) {
-		await ns.sleep(100);
-	}
+	const pid = ns.exec(ns.getScriptName(), target, 1, ns.args[0]);
+	await waitTargetPid(ns, pid, target);
 }
 
+/**
+ * @param {NS} ns
+ * @param {string} target
+ */
 async function own(ns, target) {
-	if (ns.getServerNumPortsRequired(target) == 0) {
-		ns.nuke(target);
-	} else if (ns.getServerNumPortsRequired(target) == 1) {
-		ns.brutessh(target);
-		ns.nuke(target);
-	}
-	else {
+	const portsRequired = ns.getServerNumPortsRequired(target);
+	if (portsRequired > 2) {
 		return;
 	}
-	if (ns.fileExists("crawler.js", target)) return;
-	await ns.scp("harvest.js", target);
-	await ns.scp("crawler.js", target);
-	
+
+	if (portsRequired >= 2) {
+		if (!ns.fileExists("FTPCrack.exe", "home")) {
+			return;
+		}
+		ns.ftpcrack(target);
+	}
+	if (portsRequired >= 1) {
+		if (!ns.fileExists("BruteSSH.exe", "home")) {
+			return;
+		}
+		ns.brutessh(target);
+	}
+	if (portsRequired >= 0) {
+		ns.nuke(target);
+	}
+
+	if (ns.fileExists(ns.getScriptName(), target)) return;
+
+	await deploy(ns, target);
 	await spread(ns, target);
 }
 
+/**
+ * @param {NS} ns
+ * @param {string} target
+ */
 async function startHack(ns, target) {
 	const availableRam = ns.getServerMaxRam(target) - ns.getServerUsedRam(target)
-	const harvestRam = 2.4;
+	const harvestRam = ns.getScriptRam("harvest.js");
 	const threads = Math.floor(availableRam / harvestRam);
 	if (threads) {
-		ns.exec("harvest.js", target, threads, target);
-	} 
-	else {
+		ns.exec("harvest.js", target, 1, target);
+	} else {
 		ns.tprint("[ERROR]: ", target, " has no available RAM");
 	}
 }
 
+/**
+ * @param {NS} ns
+ * @param {string} target
+ */
 async function killall_target(ns, target) {
 	ns.killall(target);
 	await spread(ns, target);
-	ns.rm("crawler.js", target);
+	ns.rm(ns.getHostname(), target);
 }
 
+/**
+ * @param {NS} ns
+ */
 export async function main(ns) {
 	if (ns.args.length != 1) {
-		ns.tprint("Usage: HACK|KILLALL");
+		ns.tprint("Usage: HACK|KILLALL|DEPLOY");
 		ns.exit(1);
 	}
 	const operation = ns.args[0];
@@ -51,7 +79,7 @@ export async function main(ns) {
 
 	ns.tprint(operation);
 	for(var i = 0; i < adjacents.length; i++ ) {
-		const target = adjacents[i]
+		const target = adjacents[i];
 		if (target.substring(0, 5) == "home-" || target == "home") continue;
 		ns.tprint("Starting: ", target);
 		await own(ns, target);
@@ -59,6 +87,10 @@ export async function main(ns) {
 			await startHack(ns, target);
 		} else if (operation.toLowerCase() == "killall") {
 			await killall_target(ns, target);
+		} else if (operation.toLowerCase() == "deploy") {
+			continue;
+		} else {
+
 		}
 		ns.tprint("Done: ", target);
 	}
