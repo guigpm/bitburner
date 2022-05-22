7f35c58fc56f5f96992b1e188cbcf2ee102c6241
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 22:58:40 -0300

    targeted.js with lib and log

--- targeted.js
+++ targeted.js
@@ -1,31 +1,14 @@
+import { canBeHacked, disableFunctionLog } from 'lib.js';
+import { log } from 'log.js';
+
+let activeExecuters = ['home'];
+
 /** @param {NS} ns */
-let activeExecuters = [];
-
-
-function log(ns, target, message) {
-	ns.tprint(target, " ", message);
-	ns.print(target, " ", message);
-}
-
-function validTarget(ns, target) {
-	const alreadyRunning = ns.scriptRunning("harvest.js", target);
-	const moneyAvailable = ns.getServerMoneyAvailable(target) > 0;
-	const rootAccess = ns.hasRootAccess(target);
-	const hackLevel = ns.getServerRequiredHackingLevel(target) <= ns.getHackingLevel();
-
-	alreadyRunning && log(ns, target, "is already being hacked");
-	!moneyAvailable && log(ns, target, "has no money");
-	!rootAccess && log(ns, target, "doesnt have root access");
-	!hackLevel && log(ns, target, "hack level is above mine");
-
-	return !alreadyRunning && moneyAvailable && rootAccess && hackLevel;
-}
-
 export async function main(ns) {
-	ns.disableLog("sleep");
+	disableFunctionLog(ns, "sleep");
 	const servers = ns.scan();
 	let pids = [];
-	activeExecuters = servers.filter((server) => server.substring(0, 5) == "home-");
+	// activeExecuters = servers.filter((server) => server.substring(0, 5) == "home-");
 	const targets = servers.filter((server) => server.substring(0, 5) != "home-" && server != "home");
 	ns.tprint(targets);
 	for (var i = 0; i < targets.length; i++) {
@@ -39,9 +22,9 @@ export async function main(ns) {
 			}
 			await ns.sleep(1000);
 		}
-		if (validTarget(ns, target)) {
-			log(ns, target, "Valid");
+		if (canBeHacked(ns, target)) {
 			const executer = activeExecuters.pop();
+			log.info(ns, `Will break ${target}`, executer);
 			const pid = ns.run("breakServer.js", 1, executer, target);
 			pids.push({"pid": pid, "executer": executer, "active": true});
 		}
