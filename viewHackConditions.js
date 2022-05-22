e57e5382313f6e2414a3811b550f56a3bc33c26a
Guilherme Mattioli <guigpm@hotmail.com>
2022-05-22 04:30:14 -0300

    file to view all conditions in trace mode

---
+++ viewHackConditions.js
@@ -0,0 +1,40 @@
+import { canBeHacked, disableFunctionLog } from './lib.js';
+import { log, logLevel } from './log.js';
+
+let activeExecuters = [];
+
+/** @param {import("./NameSpace").NS} ns */
+export async function main(ns) {
+  if (ns.args.length < 1) {
+      log.fatal(ns, "Usage: target [logLevel] [from]");
+      ns.exit(1);
+  }
+  const target = ns.args[0];
+  log.logLevel = ns.args[1] ?? logLevel.trace;
+  let serverFrom = ns.args[2] ?? null;
+  if (serverFrom) {
+    serverFrom = `[From: ${serverFrom}] `;
+  } else {
+    serverFrom = '';
+  }
+
+  const moneyCurrent = ns.getServerMoneyAvailable(target);
+  const moneyMax = ns.getServerMaxMoney(target);
+  const moneyThresh = moneyMax * 0.75;
+  log.trace(ns, serverFrom + '---Money---', target);
+  log.trace(ns, serverFrom + 'Max Server Money: ' + ns.nFormat(moneyMax, '$0.00a'), target);
+  log.trace(ns, serverFrom + 'Current Server Money: ' + ns.nFormat(moneyCurrent, '$0.00a'), target);
+  log.trace(ns, serverFrom + 'Money Threshold (0.75 * Max Server Money): ' + ns.nFormat(moneyThresh, '$0.00a'), target);
+  log.trace(ns, serverFrom + 'Condition to <grow> (Current Server Money < Money Threshold): ' +  ((moneyCurrent < moneyThresh) ? 'True' : 'False'), target);
+
+  const securityCurrent = ns.getServerSecurityLevel(target);
+  const securityMin = ns.getServerMinSecurityLevel(target);
+  const securityThresh = securityMin + 5;
+  log.trace(ns, serverFrom + '---Security---', target);
+  log.trace(ns, serverFrom + 'Min Server Security: ' + securityMin, target);
+  log.trace(ns, serverFrom + 'Current Server Security: ' + securityCurrent, target);
+  log.trace(ns, serverFrom + 'Security Threshold (Min Server Security + 5): ' + securityThresh, target);
+  log.trace(ns, serverFrom + 'Condition to <weak> (Current Server Security > Security Threshold): ' +  ((securityCurrent > securityThresh) ? 'True' : 'False'), target);
+  log.trace(ns, serverFrom + '---------', target);
+
+}
