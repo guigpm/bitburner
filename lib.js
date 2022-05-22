36c7372f3fd18e5700282b6cbe30087626559bbb
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 22:57:36 -0300

    lib.js with lib and log

--- lib.js
+++ lib.js
@@ -1,16 +1,58 @@
-import { BaseClass } from 'baseClass.js';
+import { log } from 'log.js';
 
-/** @param {NS} nameSpace */
-export class Lib extends BaseClass {
-	defaultWaitInterval = 100;
-
-	/**
-	 * @param {number} pid
-	 * @param {string} target [optional]
-	 */
-	async waitTargetPid(pid, target = undefined) {
-		while (this.nameSpace.isRunning(pid, target)) {
-			await sleep(this.defaultWaitInterval);
-		}
+/**
+ * @param {NS} ns
+ * @param {int} pid
+ * @param {string} target [optional]
+ */
+export async function waitTargetPid(ns, pid, target = undefined) {
+	disableFunctionLog(ns, 'sleep');
+	while (ns.isRunning(pid, target)) {
+		await ns.sleep(100);
 	}
 }
+
+/**
+ * @param {NS} ns
+ * @param {string} fn
+ */
+export function disableFunctionLog(ns, fn) {
+	if (ns.isLogEnabled(fn)) {
+		ns.disableLog(fn);
+	}
+}
+
+/**
+ * @param {NS} ns
+ * @param {string} target
+ * @param {string} hostName [optional]
+ */
+export async function deploy(ns, target, hostName = undefined) {
+	const sources = ns.ls(hostName ?? ns.getHostname(), '.js');
+	for (const file of sources) {
+		await ns.scp(file, target);
+	}
+}
+
+/**
+ * @remarks RAM cost 0.4 GB
+ * 
+ * @param {NS} ns
+ * @param {string} target
+ * @param {string} scriptRunningName [optional]
+ */
+export function canBeHacked(ns, target, scriptRunningName = 'harvest.js') {
+	const alreadyRunning = ns.isRunning(scriptRunningName, target, target);
+	const moneyAvailable = ns.getServerMoneyAvailable(target) > 0;
+	const rootAccess = ns.hasRootAccess(target);
+	const hackLevel = ns.getServerRequiredHackingLevel(target) <= ns.getHackingLevel();
+
+	alreadyRunning && log.info(ns, "is already being hacked", target);
+	!moneyAvailable && log.info(ns, "has no money", target);
+	!rootAccess && log.info(ns, "doesnt have root access", target);
+	!hackLevel && log.info(ns, "hack level is above mine", target);
+
+	return !alreadyRunning && moneyAvailable && rootAccess && hackLevel;
+}
+
+export function Lib() {};
