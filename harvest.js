9ba33a7c07bfe2b6b6718ed9a28eb69d1f2fe0f2
Guilherme Mattioli <djmattioli@gmail.com>
2022-05-21 22:56:50 -0300

    harvest.js  with lib and log

--- harvest.js
+++ harvest.js
@@ -1,3 +1,5 @@
+import { waitTargetPid } from 'lib.js';
+
 /** @param {NS} ns */
 export async function main(ns) {
 	if (ns.args.length != 1) {
@@ -17,17 +19,42 @@ export async function main(ns) {
 	// we'll weaken it before doing anything else
 	var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
+
+	const availableRam = ns.getServerMaxRam(target) - ns.getServerUsedRam(target);
+	const weakThreads = Math.floor(availableRam / ns.getScriptRam("weak.js"));
+	const growThreads = Math.floor(availableRam / ns.getScriptRam("grow.js"));
+	const hackThreads = Math.floor(availableRam / ns.getScriptRam("hack.js"));
+	// const threads = Math.floor(availableRam / harvestRam);
+
+
 	// Infinite loop that continously hacks/grows/weakens the target server
 	while (true) {
+		let execPID = null;
 		if (ns.getServerSecurityLevel(target) > securityThresh) {
+			if (weakThreads) {
+				execPID = ns.exec("weak.js", target, weakThreads, target);
+			}
+			
 			// If the server's security level is above our threshold, weaken it
 			await ns.weaken(target);
 		} else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+			if (growThreads) {
+				execPID = ns.exec("grow.js", target, growThreads, target);
+			}
+
 			// If the server's money is less than our threshold, grow it
 			await ns.grow(target);
 		} else {
+			if (hackThreads) {
+				execPID = ns.exec("hack.js", target, hackThreads, target);
+			}
+			
 			// Otherwise, hack it
 			await ns.hack(target);
+		}
+
+		if (execPID) {
+			await waitTargetPid(ns, execPID, target);
 		}
 	}
 }
