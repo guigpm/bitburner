6f1c3991484903b7850ca4c8101a8230e194f11a
Nikolas Moya <nikolasmoya@gmail.com>
2022-05-26 00:13:28 +0200

    Fetch contract now works

--- fetchContracts.js
+++ fetchContracts.js
@@ -1,27 +1,35 @@
 import { Queue } from "./queue";
+
+class Contract {
+    constructor(server, filename) {
+        this.server = server;
+        this.filename = filename;
+    }
+}
 /** @param {import("./NameSpace").NS} ns */
 export async function main(ns) {
     const visited = [];
-    const contracts = [];
+    const foundContracts = [];
     const queue = new Queue();
     queue.enqueue("home");
 
-    // while (!queue.isEmpty) {
-    //     const current = queue.dequeue();
-    //     visited.push(current);
+    while (!queue.isEmpty) {
+        const current = queue.dequeue();
+        visited.push(current);
 
-    //     const contracts = ns.ls(current, ".cct");
-    //     for (const contract of contracts) {
-    //         contracts.push({ "server": current, "contract": contract });
-    //     }
+        for (const contract of ns.ls(current, ".cct")) {
+            foundContracts.push(new Contract(current, contract));
+        }
+        const adjacents = ns.scan(current);
+        for (const adjacent of adjacents) {
+            if (!visited.includes(adjacent)) {
+                queue.enqueue(adjacent);
+            }
+        }
+    }
+    for (const contract of foundContracts) {
+        ns.tprint(contract.server + " " + contract.filename);
+    }
 
-    //     const adjacents = ns.scan(current);
-    //     for (const adjacent of adjacents) {
-    //         if (!visited.includes(adjacent)) {
-    //             queue.enqueue(adjacent);
-    //         }
-    //     }
-    // }
-    ns.tprint(contracts);
-    return contracts
+    return foundContracts
 }
