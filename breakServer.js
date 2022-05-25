1720f43d196de7055fcd803f0b7dac0da8978210
Guilherme Mattioli <guigpm@hotmail.com>
2022-05-25 00:44:31 -0300

    breakserver haack if no RAM on servers

--- breakServer.js
+++ breakServer.js
@@ -14,13 +14,23 @@ export async function main(ns) {
 
   const weakThreads = getMaxThreadsFromScript(ns, executer, "weak.js");
   const growThreads = getMaxThreadsFromScript(ns, executer, "grow.js");
-  const harvestThreads = 1;
+  const harvestThreads = getMaxThreadsFromScript(ns, target, "harvest.js");
+  const harvestRunning = ns.isRunning("harvest.js", target, target);
+  let pid = 0, stop = false;
 
-  let pid = 0;
+  if (harvestRunning) {
+    log.info(ns, `Already running 'harvest.js' on ${target}.`);
+  } else if (harvestThreads) {
+    pid = ns.exec("harvest.js", target, 1 /* Thread */, target);
+    log.trace(`Started 'harvest.js' on ${target} with PID=<${pid ?? 'NULL'}>.`)
+  } else {
+    log.error(ns, `Can't start 'harvest.js' on ${target}. Probably no RAM.`);
+  }
+
   log.info(ns, `Breaking server ${target} on ${executer}`);
-  while (true) {
+  while (!stop) {
     if (weakenCondition(ns, target)) {
-      log.debug(ns, "Weak " + target);
+      log.debug(ns, `Weak ${target} on ${executer}`);
       if (weakThreads) {
         pid = ns.exec("weak.js", executer, weakThreads, target);
       } else {
@@ -28,7 +38,7 @@ export async function main(ns) {
         break;
       }
     } else if (growCondition(ns, target)) {
-      log.debug(ns, "Grow " + target);
+      log.debug(ns, `Grow ${target} on ${executer}`);
       if (growThreads) {
         pid = ns.exec("grow.js", executer, growThreads, target);
       } else {
@@ -36,13 +46,14 @@ export async function main(ns) {
         break;
       }
     } else {
-      log.debug(ns, "Harvest " + target);
-      if (harvestThreads) {
-        pid = ns.exec("harvest.js", target, harvestThreads, target);
+      log.debug(ns, `Harvest ${target} on ${executer}`);
+      if (harvestRunning || harvestThreads) {
+        break;
       } else {
-        log.error(ns, `Can't harvest on ${target}. Probably no RAM.`);
+        pid = ns.exec("hack.js", executer, getMaxThreadsFromScript(ns, executer, "hack.js"), target);
+        log.info(ns, `Hacking on ${executer}.`);
+        stop = true;
       }
-      break;
     }
     while (pid && ns.isRunning(pid, executer)) {
       await ns.sleep(500);
