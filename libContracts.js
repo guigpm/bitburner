af102782831aea6fd0d1f868374d5711b4d4b55c
Nikolas Moya <nikolasmoya@gmail.com>
2022-06-13 23:21:12 +0200

    Add generate IP Addressess

--- libContracts.js
+++ libContracts.js
@@ -19,6 +19,8 @@ export class Contract extends BaseContext {
     }
     static factory(contractType) {
         switch (contractType) {
+            case "Generate IP Addresses":
+                return GenerateIPAddresses;
             case "Unique Paths in a Grid I":
                 return UniquePathsInAGridI;
             case "Unique Paths in a Grid II":
@@ -76,6 +78,41 @@ export class Contract extends BaseContext {
         else {
             this.ctx.log.warning(`Contract not submitted, not enough attempts left.`);
         }
+    }
+}
+
+class GenerateIPAddresses extends Contract {
+    /*
+    Given the following string containing only digits, return an array with all possible valid IP address combinations that can be created from the string:
+    10196184245
+
+    Note that an octet cannot begin with a '0' unless the number itself is actually 0. For example, '192.168.010.1' is not a valid IP.
+
+    Examples:
+
+    25525511135 -> [255.255.11.135, 255.255.111.35]
+    1938718066 -> [193.87.180.66]
+    */
+    solution(_input) {
+        const data = String(_input);
+        const len = data.length - 3;
+        var res = [];
+
+        for (let a = 0; a < 3 && a < len; ++a) {
+            for (let b = 0; b < 3 && b < len - a; ++b) {
+                for (let c = 0; c < 3 && c < len - a - b; ++c) {
+                    const A = parseInt(data.substring(0, a + 1), 10);
+                    const B = parseInt(data.substring(a + 1, a + b + 2), 10);
+                    const C = parseInt(data.substring(a + b + 2, a + b + c + 3), 10);
+                    const D = parseInt(data.substring(a + b + c + 3), 10);
+
+                    if (A < 256 && B < 256 && C < 256 && D < 256)
+                        res.push(A + "." + B + "." + C + "." + D);
+                }
+            }
+        }
+
+        return res;
     }
 }
 
